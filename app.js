3c86f973e091fb0a413d1b3985529b5a25b983db
Ren22 <ngm.renat@gmail.com>
2021-08-31 01:21:21 +0200

    sprint_1: serve static from dist with express

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PORT = 3000;
 const app = express();
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(PORT, () => {
   console.log(`App is running at ${PORT}!`);
