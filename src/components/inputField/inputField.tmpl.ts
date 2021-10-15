29728b93e9b751926a092fcd6ef3ce09b859ede9
Ren22 <ngm.renat@gmail.com>
2021-10-16 00:38:48 +0200

    sprint 2: minore adjustments

--- src/components/inputField/inputField.tmpl.ts
+++ src/components/inputField/inputField.tmpl.ts
@@ -1,17 +1,19 @@
 export default `
-<div 
-  class="{{#if mediumMarginHorizontally}}mediumMarginLeft mediumMarginRight {{/if}}
-  {{#if vbox}}inputfieldVBox{{else}}inputfieldHBox{{/if}}
-  {{#if justifyContentSpaceBetween}}justifyContentSpaceBetween{{/if}}">
-  <label for="{{inputFieldId}}" class="{{labelStyle}}">{{inputFieldText}}</label>
-  <input 
-    placeholder="{{inputFieldPlaceholder}}" 
-    id="{{inputFieldId}}" 
-    type="{{inputFieldType}}" 
-    value="{{inputFieldValue}}" 
-    name="{{inputFieldText}}"
-    class="{{inpFieldStyle}}"
-    {{#if readOnly}}readonly{{/if}}>
-  <span class="error-message"></span>
+<div>
+  <div 
+    class="{{#if mediumMarginHorizontally}}mediumMarginLeft mediumMarginRight {{/if}}
+    {{#if vbox}}inputfieldVBox{{else}}inputfieldHBox{{/if}}
+    {{#if justifyContentSpaceBetween}}justifyContentSpaceBetween{{/if}}">
+    <label for="{{inputFieldId}}" class="{{labelStyle}}">{{inputFieldText}}</label>
+    <input 
+      placeholder="{{inputFieldPlaceholder}}"
+      id="{{inputFieldId}}"
+      type="{{inputFieldType}}"
+      value="{{inputFieldValue}}"
+      name="{{inputFieldText}}"
+      class="{{inpFieldStyle}}"
+      {{#if readOnly}}readonly{{/if}}>
+  </div>
+  <span class="error-message mediumMarginLeft mediumMarginRight"></span>
 </div>
 `;
