a0596d47d2f0ba824bb920743e33500556ac0985
Ren22 <ngm.renat@gmail.com>
2021-12-03 01:59:09 +0100

    sprint 4: remove parcel and use webpack instead

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'coolchat.bundle.js',
+    publicPath: '',
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json'],
+    alias: {
+      handlebars: 'handlebars/dist/handlebars.js',
+    },
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './index.html',
+    scriptLoading: 'module',
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
+            },
+          },
+        ],
+        exclude: /(node_modules)/,
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                strictMath: true,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
