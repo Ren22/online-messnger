6b63ce97c7c0dc5548d7957e1c73f222e42c0b10
Ren22 <ngm.renat@gmail.com>
2021-12-04 23:53:42 +0100

    sprint 4: add eslint to pre-commit

--- src/utils/router.test.ts
+++ src/utils/router.test.ts
@@ -18,7 +18,8 @@ const { JSDOM } = jsdom;
 //   const originalForwards = history.forward;
 
 //   // eslint-disable-next-line no-proto
-//   (history as unknown as {__proto__: History}).__proto__.back = function patchedBack(this: History, ...args: Parameters<History['back']>): void {
+//   (history as unknown as {__proto__: History}).__proto__.back =
+//  function patchedBack(this: History, ...args: Parameters<History['back']>): void {
 //     originalBack.apply(this, args);
 
 //     const popStateEvent = document.createEvent('Event');
@@ -27,7 +28,8 @@ const { JSDOM } = jsdom;
 //   };
 
 //   // eslint-disable-next-line no-proto
-//   (history as unknown as {__proto__: History}).__proto__.forward = function patchedForward(this: History, ...args: Parameters<History['forward']>): void {
+//   (history as unknown as {__proto__: History}).__proto__.forward =
+//  function patchedForward(this: History, ...args: Parameters<History['forward']>): void {
 //     originalForwards.apply(this, args);
 
 //     const popStateEvent = document.createEvent('Event');
